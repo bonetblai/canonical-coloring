7d633f5b5f6f98bfc06aacc12673190103741faa
Blai Bonet <bonetblai@gmail.com>
2024-05-31 15:58:52 -0400

    Add operator<< for pairs

--- src/util.h
+++ src/util.h
@@ -3,13 +3,20 @@
 #include <vector>
 
 // Forward reference
+template<typename U, typename V> std::ostream& operator<<(std::ostream &os, const std::pair<U, V> &v);
 template<typename T> std::ostream& operator<<(std::ostream &os, const std::set<T> &v);
+
+template<typename U, typename V>
+std::ostream& operator<<(std::ostream &os, const std::pair<U, V> &p) {
+    os << "(" << p.first << "," << p.second << ")" << std::flush;
+    return os;
+}
 
 template<typename T>
 std::ostream& operator<<(std::ostream &os, const std::vector<T> &v) {
     os << "[";
     for( size_t i = 0; i < v.size(); ++i ) {
-        os << v.at(i);
+        os << v.at(i) << std::flush;
         if( i + 1 < v.size() ) os << " ";
     }
     os << "]";
@@ -17,12 +24,12 @@ std::ostream& operator<<(std::ostream &os, const std::vector<T> &v) {
 }
 
 template<typename T>
-std::ostream& operator<<(std::ostream &os, const std::set<T> &v) {
+std::ostream& operator<<(std::ostream &os, const std::set<T> &s) {
     os << "{";
     size_t i = 0;
-    for( typename std::set<T>::const_iterator it = v.begin(); it != v.end(); ++it ) {
-        os << *it;
-        if( i++ + 1 < v.size() ) os << " ";
+    for( typename std::set<T>::const_iterator it = s.begin(); it != s.end(); ++it ) {
+        os << *it << std::flush;
+        if( i++ + 1 < s.size() ) os << " ";
     }
     os << "}";
     return os;
